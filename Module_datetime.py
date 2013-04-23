6baaced575cb80adac62a4bb2a29dcebaf943d9b
zhanghailu <zhlccna@163.com>
2013-04-24 02:42:47 +0800

    change

--- Module_datetime.py
+++ Module_datetime.py
@@ -3,11 +3,13 @@
 
 import sys
 import datetime
+import getinfo
 
 #now = datetime.datetime.now()
 #Date = now.strftime('%Y-%m-%d %H:%M:%S')
 #print Date
 
+'''init date => version if version has same change,then change the value init'''
 change_d = '2013-04-26 00:00:00'
 init_d = datetime.datetime.strptime(change_d,'%Y-%m-%d %H:%M:%S')
 init_version = 85
@@ -15,11 +17,9 @@ init_version = 85
 
 
 
-
-
 def count_Along(open_day_str):
-	#open_day = datetime.datetime.strptime(open_day_str,'%Y-%m-%d %H:%M:%S')
-	open_day = datetime.datetime.strptime(open_day_str,'%Y-%m-%d')
+	open_day = datetime.datetime.strptime(open_day_str,'%Y-%m-%d %H:%M:%S')
+	#open_day = datetime.datetime.strptime(open_day_str,'%Y-%m-%d')
 	Along = open_day - init_d
 	return Along
 
@@ -29,19 +29,19 @@ def count_Along(open_day_str):
 def select_version(Days):
 	if Days > 0:
 		print 'After now version,%d days' %(Days)
-		add_ver = Days / 7
-		version = init_version + add_ver
+		change_ver = Days / 7
+		version = init_version + change_ver
 		return version
 	else:
 		Days = - Days
 		print 'Before now version,%d days' %(Days)
-		reduce_ver = Days / 7
-		version = init_version - reduce_ver
+		change_ver = Days / 7
+		version = init_version - change_ver
 		return version
 
 
 if __name__ == '__main__':
-	open_day_str = '2013-04-19'
+	open_day_str = getinfo.getzoneinfo('xd','s20','/Users/hyperion/shell/serverInfo.cfg','opentime')
 	Along = count_Along(open_day_str)
 	version = select_version(Along.days)
 	print "v%d" %(version)
